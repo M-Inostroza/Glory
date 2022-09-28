287e898362d0e034f81eab85f69377bda43821a7
Mauricio Inostroza <mautrode@gmail.com>
2022-09-28 21:01:12 +0200

    target anim update

--- Glory or Death/Assets/Code/Target.cs
+++ Glory or Death/Assets/Code/Target.cs
@@ -5,55 +5,27 @@ using DG.Tweening;
 
 public class Target : MonoBehaviour
 {
-    public GameObject timer;
-
-    //Parameters
-    private float minSize;
-    public float speed;
-
     //Anim
-    Tween timerAnim;
-    
-    SpriteRenderer render;
     Animator anim;
-
-    //Effect
-    public GameObject hitEffect;
+    public GameObject timer;
 
     //BattleSystem
     public BattleSystem BS;
 
     private void Start()
     {
-        render = gameObject.GetComponent<SpriteRenderer>();
-        anim = hitEffect.GetComponent<Animator>();
-        minSize = 1f;
-    }
-    private void OnEnable()
-    {
-        render.DOFade(1, 0.1f);
-        timerAnim = timer.transform.DOScale(minSize, speed).OnComplete(() => gameObject.SetActive(false));
-    }
-
-    private void OnDisable()
-    {
-        timerAnim.Rewind();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     private void OnMouseDown()
     {
+        anim.SetBool("hit", true);
         timer.SetActive(false);
-        render.DOFade(0, 0.1f);
-        anim.Play(0);
         BS.targetHit++;
-
-        StartCoroutine(killTarget());
     }
 
-
-    IEnumerator killTarget()
+    void killTarget()
     {
-        yield return new WaitForSeconds(1);
         gameObject.SetActive(false);
     }
 }

ed5770fd5301b6d63563ca84a2d0622a5dc60836
Mauricio Inostroza <mautrode@gmail.com>
2022-10-10 21:44:37 +0200

    arrow mechanic update

--- Glory or Death/Assets/DefendManager.cs
+++ Glory or Death/Assets/DefendManager.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class DefendManager : MonoBehaviour
 {
-    public GameObject[] arrows;
+    public GameObject[] arrowPrefabs;
     float intPos = -70;
-    public int[] rotationValues;
-    
+
+    public GameObject randomArrow;
+    public GameObject arrow;
     private void OnEnable()
     {
-        Vector3 rotate = new Vector3(0, 0, 0);
-        for (int i = 0; i < arrows.Length; i++)
+        for (int i = 0; i < 4; i++)
         {
-            GameObject arrow = Instantiate(arrows[Random.Range(0, 4)], new Vector2(0, 0), Quaternion.Euler(new Vector3(0,0, rotationValues[Random.Range(0,4)])));
+            randomArrow = arrowPrefabs[Random.Range(0, 4)];
+            arrow = Instantiate(randomArrow, new Vector2(gameObject.transform.position.x + intPos, gameObject.transform.position.y), randomArrow.transform.rotation);
             arrow.transform.SetParent(gameObject.transform);
             arrow.transform.localScale = new Vector3(1, 1, 1);
-            arrow.transform.SetPositionAndRotation(new Vector2(gameObject.transform.position.x + intPos, gameObject.transform.position.y), Quaternion.identity);
             intPos += 45f;
         }  
+    }
+
+    private void OnDisable()
+    {
+        intPos = -70;
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }

f921975695b59c84d9f4b368f9a1103b9d807113
Mauricio Inostroza <mautrode@gmail.com>
2022-10-12 22:00:05 +0200

    update arrow mechanic

--- Glory or Death/Assets/DefendManager.cs
+++ Glory or Death/Assets/DefendManager.cs
@@ -1,32 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DefendManager : MonoBehaviour
 {
     public GameObject[] arrowPrefabs;
-    float intPos = -70;
+    float intPos;
 
-    public GameObject randomArrow;
-    public GameObject arrow;
+    List<GameObject> instantArrows = new List<GameObject>();
+
     private void OnEnable()
     {
+        intPos = -70;
         for (int i = 0; i < 4; i++)
         {
-            randomArrow = arrowPrefabs[Random.Range(0, 4)];
-            arrow = Instantiate(randomArrow, new Vector2(gameObject.transform.position.x + intPos, gameObject.transform.position.y), randomArrow.transform.rotation);
-            arrow.transform.SetParent(gameObject.transform);
-            arrow.transform.localScale = new Vector3(1, 1, 1);
+            GameObject randomArrow = arrowPrefabs[Random.Range(0, 4)];
+            instantArrows.Add(Instantiate(randomArrow, new Vector2(gameObject.transform.position.x + intPos, gameObject.transform.position.y), randomArrow.transform.rotation));
+            instantArrows[i].transform.SetParent(gameObject.transform);
+            instantArrows[i].transform.localScale = new Vector3(1, 1, 1);
             intPos += 45f;
-        }  
+        }
+    }
+
+    private void Update()
+    {
+        pressCommands();
     }
 
     private void OnDisable()
     {
-        intPos = -70;
         foreach (Transform child in transform)
         {
+            instantArrows.Clear();
             Destroy(child.gameObject);
         }
+    }
+
+    void pressCommands()
+    {
+        if(instantArrows.Count > 0)
+        {
+            switch (instantArrows[0].name)
+            {
+                case "down(Clone)":
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        killArrow(instantArrows[0]);
+                    }
+                    break;
+                case "up(Clone)":
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        killArrow(instantArrows[0]);
+                    }
+                    break;
+                case "left(Clone)":
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        killArrow(instantArrows[0]);
+                    }
+                    break;
+                case "right(Clone)":
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    {
+                        killArrow(instantArrows[0]);
+                    }
+                    break;
+            }
+        } else
+        {
+            gameObject.SetActive(false);
+        }
+        
+    }
+
+
+    void killArrow(GameObject arrow)
+    {
+        Destroy(arrow.gameObject);
+        instantArrows.RemoveAt(0);
     }
 }

6f63e2b949c74de81eb6cb88fdc1864bd91ece24
Mauricio Inostroza <mautrode@gmail.com>
2022-10-16 21:06:45 +0200

    update anims commands

--- Glory or Death/Assets/DefendManager.cs
+++ Glory or Death/Assets/DefendManager.cs
@@ -5,13 +5,23 @@ using DG.Tweening;
 
 public class DefendManager : MonoBehaviour
 {
+    public Player playerUnit;
     public GameObject[] arrowPrefabs;
     float intPos;
+    float timer;
+
+    int extraAgility;
 
     List<GameObject> instantArrows = new List<GameObject>();
-
+   
     private void OnEnable()
     {
+        timer = 1.5f;
+
+        // Resets player's agility after buff
+        playerUnit.currentAgility -= extraAgility;
+        extraAgility = 0;
+        
         intPos = -70;
         for (int i = 0; i < 4; i++)
         {
@@ -21,6 +31,9 @@ public class DefendManager : MonoBehaviour
             instantArrows[i].transform.localScale = new Vector3(1, 1, 1);
             intPos += 45f;
         }
+
+        // Sets timer
+        StartCoroutine(commandTimer());
     }
 
     private void Update()
@@ -30,6 +43,7 @@ public class DefendManager : MonoBehaviour
 
     private void OnDisable()
     {
+        playerUnit.currentAgility += extraAgility;
         foreach (Transform child in transform)
         {
             instantArrows.Clear();
@@ -47,12 +61,18 @@ public class DefendManager : MonoBehaviour
                     if (Input.GetKeyDown(KeyCode.DownArrow))
                     {
                         killArrow(instantArrows[0]);
+                    } else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        failArrow(instantArrows[0]);
                     }
                     break;
                 case "up(Clone)":
                     if (Input.GetKeyDown(KeyCode.UpArrow))
                     {
                         killArrow(instantArrows[0]);
+                    } else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        failArrow(instantArrows[0]);
                     }
                     break;
                 case "left(Clone)":
@@ -60,17 +80,25 @@ public class DefendManager : MonoBehaviour
                     {
                         killArrow(instantArrows[0]);
                     }
+                    else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        failArrow(instantArrows[0]);
+                    }
                     break;
                 case "right(Clone)":
                     if (Input.GetKeyDown(KeyCode.RightArrow))
                     {
                         killArrow(instantArrows[0]);
                     }
+                    else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        failArrow(instantArrows[0]);
+                    }
                     break;
             }
         } else
         {
-            gameObject.SetActive(false);
+            StartCoroutine(animwait());
         }
         
     }
@@ -78,7 +106,28 @@ public class DefendManager : MonoBehaviour
 
     void killArrow(GameObject arrow)
     {
-        Destroy(arrow.gameObject);
+        extraAgility++;
         instantArrows.RemoveAt(0);
+        arrow.transform.DOLocalJump(new Vector2(arrow.transform.localPosition.x, arrow.transform.localPosition.y + 10), 6, 1, 0.3f).OnComplete(() => Destroy(arrow.gameObject));
+    }
+
+    void failArrow(GameObject arrow)
+    {
+        extraAgility--;
+        instantArrows.RemoveAt(0);
+        arrow.transform.DOShakePosition(0.3f, 4, 20).OnComplete(() => Destroy(arrow.gameObject));
+    }
+
+
+    IEnumerator commandTimer()
+    {
+        yield return new WaitForSeconds(timer);
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator animwait()
+    {
+        yield return new WaitForSeconds(0.3f);
+        gameObject.SetActive(false);
     }
 }

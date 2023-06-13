960f77cc754798d45b4d23f264c84a881da3ddd0
Mauricio Inostroza <mautrode@gmail.com>
2023-06-13 20:37:23 +0200

    Update focus

--- Glory or Death/Assets/Code/Systems/focusManager.cs
+++ Glory or Death/Assets/Code/Systems/focusManager.cs
@@ -7,12 +7,18 @@ using UnityEngine.UI;
 public class focusManager : MonoBehaviour
 {
     // Speed at which the cursor sprite moves
-    private float cursorDuration = 1f;
-    private float targetSpeed = 1f;
+    private float cursorDuration = 2f;
+    private float targetSpeed = 0.6f;
 
     // Cursor sprite and target sprite
     public GameObject cursor;
     public GameObject target;
+
+
+    [SerializeField]
+    private GameObject playerUnit;
+
+    private bool isHit;
 
     // Set limits of bar
     private float minX = -8.5f;
@@ -22,8 +28,11 @@ public class focusManager : MonoBehaviour
     private float targetRangeMin;
     private float targetRangeMax;
 
+    timeManager timeManager;
     private void Start()
     {
+        timeManager = FindObjectOfType<timeManager>();
+
         // Set the initial position of the cursor to the right side of the screen
         cursor.transform.localPosition = new Vector2(maxX, cursor.transform.localPosition.y);
 
@@ -33,6 +42,8 @@ public class focusManager : MonoBehaviour
 
     private void OnEnable()
     {
+        isHit = true;
+
         // Minigame timer
         StartCoroutine(focusTimer());
 
@@ -41,7 +52,7 @@ public class focusManager : MonoBehaviour
         targetRangeMax = targetRangeMin + 3.5f;
 
         // Set the position of the target sprite
-        target.transform.localPosition = new Vector2(targetRangeMin, 0);
+        target.transform.localPosition = new Vector2(targetRangeMin, -14.85f);
     }
 
     private void Update()
@@ -55,12 +66,56 @@ public class focusManager : MonoBehaviour
             // Reverse the direction of the cursor sprite
             targetSpeed *= -1;
         }
+
+        checkFocus();
     }
 
     IEnumerator focusTimer() // Mejorable!!
     {
-        yield return new WaitForSeconds(2.4f);
-        FindObjectOfType<Player>().GetComponent<Animator>().SetBool("DG_Skill_Fail", true);
+        yield return new WaitForSeconds(8);
+        FindObjectOfType<Player>().GetComponent<Animator>().SetBool("skillFail", true);
         gameObject.SetActive(false);
+    }
+
+
+    void checkFocus()
+    {
+        if (Input.GetKey(KeyCode.A) && isHit)
+        {
+            isHit = false;
+
+            float cursorXstart = cursor.transform.localPosition.x;
+            float cursorXend = cursorXstart + 0.8f;
+
+            Debug.Log("Cursor start: " + cursorXstart);
+            Debug.Log("Cursor end: " + cursorXend);
+
+            Debug.Log("Target position: " + target.transform.localPosition.x);
+
+            if (target.transform.localPosition.x < cursorXstart && target.transform.localPosition.x > cursorXend)
+            {
+                Debug.Log("Is a match!");
+            } else
+            {
+                Debug.Log("Is not a match!");
+            }
+        }
+    }
+
+    void checkHit()
+    {
+        if (isHit)
+        {
+            timeManager.enemyActionIcon.sprite = timeManager.iconSprites[0];
+            playerUnit.GetComponent<Animator>().SetBool("FC_Skill", true);
+            gameObject.SetActive(false);
+            playerUnit.GetComponent<Player>().StartCoroutine(playerUnit.GetComponent<Player>().boostSpeed());
+        }
+        else
+        {
+            timeManager.enemyActionIcon.sprite = timeManager.iconSprites[1];
+            playerUnit.GetComponent<Animator>().SetBool("DG_Skill_Fail", true);
+            gameObject.SetActive(false);
+        }
     }
 }

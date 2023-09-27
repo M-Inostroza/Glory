82f1d1aab811891c0655add3cfd7ba67eb76a30d
Mauricio Inostroza <mautrode@gmail.com>
2023-09-27 20:29:46 +0200

    Upgrade screen

--- Glory or Death/Assets/upgradeManager.cs
+++ Glory or Death/Assets/upgradeManager.cs
@@ -8,8 +8,24 @@ public class upgradeManager : MonoBehaviour
     [SerializeField] GameObject _centerBlock;
     [SerializeField] GameObject _rightBlock;
 
+    List<GameObject> blockList = new List<GameObject>();
+
     private void Start()
     {
-        
+        getBlocks();
+        setRandomUpgrade();
+    }
+    private void setRandomUpgrade()
+    {
+        foreach (GameObject block in blockList)
+        {
+            block.transform.GetChild(Random.Range(0, blockList.Count -1)).gameObject.SetActive(true);
+        }
+    }
+    void getBlocks()
+    {
+        blockList.Add(_leftBlock);
+        blockList.Add(_centerBlock);
+        blockList.Add(_rightBlock);
     }
 }

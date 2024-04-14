ef53d068dcf84bcbdb0e9638d9fbac80d2e1c2eb
Mauricio Inostroza <mautrode@gmail.com>
2024-04-14 18:13:51 +0200

    update

--- Glory or Death/Assets/Code/Dialogue System/HistoryControl.cs
+++ Glory or Death/Assets/Code/Dialogue System/HistoryControl.cs
@@ -17,16 +17,33 @@ public class HistoryControl : MonoBehaviour
     private Image _framePlayer;
     private TMP_Text _textPlayer;
 
+    /* Enemy */
+    private Image _frameEnemy;
+    private TMP_Text _textEnemy;
+
+
+    /* Control history */
+    bool _storyIndex;
+    /*
+     - 0 = Presentation
+     */
+
     void Start()
     {
         TimeManager = FindObjectOfType<TimeManager>();
         _framePlayer = _panelPlayer.GetChild(1).GetComponent<Image>();
         _textPlayer = _panelPlayer.GetChild(1).transform.GetChild(0).GetComponent<TMP_Text>();
 
+        _frameEnemy = _panelEnemy.GetChild(0).GetComponent<Image>();
+        _textEnemy = _panelEnemy.GetChild(0).transform.GetChild(0).GetComponent<TMP_Text>();
+
         TimeManager.StopTime();
 
-        StartCoroutine(Interact(0, true, 1));
+        StartCoroutine(Interact(1, true, 1));
+
     }
+
+
 
     public IEnumerator Interact(int unit, bool inOut, float delay = 0, string text = "Test")
     {
@@ -40,13 +57,39 @@ public class HistoryControl : MonoBehaviour
         {
             if (unit == 0)
             {
-                _panelPlayer.gameObject.SetActive(true);
-
-                _framePlayer.DOFade(1, 0.2f);
-                _textPlayer.DOFade(1, 0.2f);
-                _panelPlayer.transform.DOLocalMoveX(250, speed);
+                ActivatePlayer(0.2f, speed);
+            }
+            else
+            {
+                ActivateEnemy(0.2f, speed, true);
             }
         }
     }
 
+    void ActivatePlayer(float fadeSpeed, float moveSpeed)
+    {
+        _panelPlayer.gameObject.SetActive(true);
+
+        _framePlayer.DOFade(1, fadeSpeed);
+        _textPlayer.DOFade(1, fadeSpeed);
+        _panelPlayer.transform.DOLocalMoveX(250, moveSpeed);
+    }
+
+    void ActivateEnemy(float fadeSpeed, float moveSpeed, bool inOut)
+    {
+        if (inOut)
+        {
+            _panelEnemy.gameObject.SetActive(true);
+
+            _frameEnemy.DOFade(1, fadeSpeed);
+            _textEnemy.DOFade(1, fadeSpeed);
+            _panelEnemy.transform.DOLocalMoveX(0, moveSpeed);
+        }
+        else
+        {
+            _frameEnemy.DOFade(0, fadeSpeed);
+            _textEnemy.DOFade(0, fadeSpeed);
+            _panelEnemy.transform.DOLocalMoveX(-260, moveSpeed).OnComplete(()=> _panelEnemy.gameObject.SetActive(true));
+        }
+    }
 }
